706c5b35b3d05db9e8f8bbe403f0243baef48569
manirathnam5 <manirathnam501@gmail.com>
2026-07-26 23:26:26 +0530

    demo

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -5,5 +5,7 @@ public class Demo {
     public static void main(String[] arg){
         System.out.println(" java 17 features ");
 
+        System.out.println("Sealed Classes introduced in java17 ");
+
     }
-}
+ }
