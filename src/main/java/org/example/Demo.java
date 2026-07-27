3d1d850579ce57770607b266912f3b7a6a125c97
manirathnam5 <manirathnam501@gmail.com>
2026-07-27 22:43:55 +0530

    demo

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -2,10 +2,19 @@ package org.example;
 
 public class Demo {
 
-    public static void main(String[] arg){
+    /*public static void main(String[] arg){
         System.out.println(" java 17 features ");
 
-        System.out.println("Sealed Classes introduced in java17 ");
+        System.out.println("Sealed Classes   introduced in java17 ");
+
+    }*/
+
+    void main(String[] arg){
+        System.out.println("Hello World ");
+
+    IO.println("Welcome To Java 25");
+    IO.println(("Y Combinator "));
 
     }
+
  }
