eb4c7cab4eb47ee7d62e7f370d3dcd937b3654e0
manirathnam5 <manirathnam501@gmail.com>
2026-07-28 21:20:04 +0530

    java features

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -12,8 +12,8 @@ public class Demo {
     void main(String[] arg){
         System.out.println("Hello World ");
 
-    IO.println("Welcome To Java 25");
-    IO.println(("Y Combinator "));
+        IO.println("Welcome To Java 25 ");
+        IO.println(("Y Combinator "));
 
     }
 
